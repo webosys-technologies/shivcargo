d1199d287e06f96b9c2533bd6bcfc7acd5dd77be
Sunil Khandare <sunil@webosys.com>
2018-03-11 18:01:19 +0530

    Statement Work Done

--- pages/statement.php
+++ pages/statement.php
@@ -37,7 +37,7 @@ th, td { min-width: 50px;
 									<input type="hidden" name="do" value="statement">
 										<?php if(isset($msg)) echo $msg;?>
                                        <center> <span class="section"><b>STATEMENT REPORT</b></span></center>
-										<div class="item form-group">  
+<!--										<div class="item form-group">  
                                                                                     <div class="row">
                                             <div class="col-md-3 col-sm-3 col-xs-12">
                                                                                 <label>LR Number</label> 
@@ -47,20 +47,29 @@ th, td { min-width: 50px;
                                                                                 <label>Date</label> 
                                                                                 <input id="name" class="form-control col-md-7 col-xs-12"  name="date" value="<?php echo $date; ?>"  type="date">
                                                                         </div>    
-                                                                                </div>  
+                                                                                </div>  -->
                                                                                     <div class="row">
-                                                                                        &nbsp;
+                                                                                       <div class="item form-group"> 
+														<div class="col-md-3 col-sm-3 col-xs-12">
+															<label>Start Date</label> 
+															<input id="name" class="form-control col-md-7 col-xs-12"  name="start_date" value="<?php echo $start_date; ?>"  type="date">
+														</div>  
+														<div class="col-md-3 col-sm-3 col-xs-12">
+															<label>End Date</label> 
+															<input id="name" class="form-control col-md-7 col-xs-12"  name="end_date" value="<?php echo $end_date; ?>"  type="date">
+														</div>  
+                                        </div>
                                                                                     </div>
                                                                                     <div class="row">
-                                                                                    <div class="col-md-3 col-sm-3 col-xs-12">
+<!--                                                                                    <div class="col-md-3 col-sm-3 col-xs-12">
                                                                                 <label>GST</label> 
                                                                                 <input id="name" class="form-control col-md-7 col-xs-12"  name="gst" value="<?php echo $gst; ?>"  type="text">
-                                                    </div>
+                                                    </div>-->
                                                                                    
                                             <div class="col-md-3 col-sm-3 col-xs-12">
 											<label>Destination City</label> 
 												 <select name="bok_descityid" style="width:250px;height:35px;" id="name" >
-												 <option value="">Select Destination City</option>
+												 <option value="no">Select Destination City</option>
 												 <option value="0" <?php if($bok_descityid==0) echo "selected";?>>All City</option>
 												<?php
 												$res_descity=mysql_query("select * from des_cities");
@@ -76,7 +85,7 @@ th, td { min-width: 50px;
                                                                                     <div class="row">
                                                                                         &nbsp;
                                                                                     </div>
-                                                                                    <div class="row">
+<!--                                                                                    <div class="row">
                                                                                     <div class="col-md-3 col-sm-3 col-xs-12">
                                                                                 <label>Sender Name</label> 
                                                                                 <input id="name" class="form-control col-md-7 col-xs-12"  name="s_name" value="<?php echo $s_name; ?>"  type="text">
@@ -87,7 +96,7 @@ th, td { min-width: 50px;
 												  <input id="name" class="form-control col-md-7 col-xs-12"  name="r_name" value="<?php echo $r_name; ?>"  type="text">
                                             </div> 
                                           
-                                                                                    </div>
+                                                                                    </div>-->
                                          
                                         <div class="ln_solid"></div>
                                         <div class="form-group">
@@ -104,15 +113,17 @@ th, td { min-width: 50px;
                     </div>
                 </div> 
 <?php
-if(isset($_GET["bok_descityid"]) || isset($_GET["gst"]) || isset($_GET["date"]) || isset($_GET["lr"]) || isset($_GET["s_name"]) || isset($_GET["r_name"]))
+if(isset($_GET["start_date"]) || isset($_GET["end_date"]) || isset($_GET["bok_descityid"]) || isset($_GET["gst"]) || isset($_GET["date"]) || isset($_GET["lr"]) || isset($_GET["s_name"]) || isset($_GET["r_name"]))
 { 
 	
-        $lr=isset($_GET["lr"]) ? addslashes($_GET["lr"]):"-";
-	$date=isset($_GET["date"]) ? addslashes($_GET["date"]):"";
-	$gst=isset($_GET["gst"]) ? addslashes($_GET["gst"]):"-";
-	$bok_descityid=isset($_GET["bok_descityid"]) ? addslashes($_GET["bok_descityid"]):"-";
-        $s_name=isset($_GET["s_name"]) ? addslashes($_GET["s_name"]):"-";
-        $r_name=isset($_GET["r_name"]) ? addslashes($_GET["r_name"]):"-";
+        $start_date=isset($_GET["start_date"]) ? addslashes($_GET["start_date"]):"";
+	$end_date=isset($_GET["end_date"]) ? addslashes($_GET["end_date"]):"";
+//        $lr=isset($_GET["lr"]) ? addslashes($_GET["lr"]):"-";
+//	$date=isset($_GET["date"]) ? addslashes($_GET["date"]):"";
+//	$gst=isset($_GET["gst"]) ? addslashes($_GET["gst"]):"-";
+	$bok_descityid=isset($_GET["bok_descityid"]) ? addslashes($_GET["bok_descityid"]):"0";
+//        $s_name=isset($_GET["s_name"]) ? addslashes($_GET["s_name"]):"-";
+//        $r_name=isset($_GET["r_name"]) ? addslashes($_GET["r_name"]):"-";
 ?>
 <script> 
 function printDiv(divName) { 
@@ -146,7 +157,7 @@ function printDiv(divName) {
                     <div class="x_content" id="printableArea">
 						 <span class="section"> 
 												<b>SHIV CARGO AGENCY</b><br/>
-												A-64 , RAM LAXMAN SANKUL , NEW COTTON MARKET ROAD<br/>
+												A-26 , RAM LAXMAN SANKUL , NEW COTTON MARKET ROAD<br/>
 												AMRAVATI PH : 0721-2590820<br/>
 												BRANCH : BUSYLAND COMPLEX NANDGAON PETH PH : 0721-	2381577<br/>
 												BRANCH : CITYLAND COMPLEX , BORGAON DHARMALE 
@@ -172,6 +183,7 @@ function printDiv(divName) {
                                     <th>Receiver GST</th>
                                     <th>City</th>
                                     <th>No of Parcel</th>
+                                    <th>Pay Mode</th>
                                     <th>Amount</th>
                                     <th>GST</th> 
                                     <th>Memo No.</th>
@@ -182,15 +194,22 @@ function printDiv(divName) {
                             </thead>
 							<tbody id="search_statement">
 							<?php  
-                                                        
-							if($bok_descityid==0)
+                                                        if($bok_descityid=="no")
+                                                        {
+                                                            $sql="select * from booking bok join des_cities dc on (bok.bok_descityid=dc.dcty_id) join des_city_place dcp on (bok.bok_cityplaceid=dcp.dcplace_id) join sender s on (bok.bok_senderid=s.sendid) join recivers r on (bok.bok_reciverid=r.recvid) where bokdate BETWEEN  '$start_date' AND '$end_date' ORDER BY boklrno AND bok_status='1'";	
+                                                        }
+							elseif($bok_descityid==0)
 							{
 								
                                                                 $sql="select * from booking bok join des_cities dc on (bok.bok_descityid=dc.dcty_id) join des_city_place dcp on (bok.bok_cityplaceid=dcp.dcplace_id) join sender s on (bok.bok_senderid=s.sendid) join recivers r on (bok.bok_reciverid=r.recvid) where bok_status='1'";	
 							}
+                                                        elseif($start_date=="" AND $end_date=="")
+                                                        {
+                                                                $sql="select * from booking bok join des_cities dc on (bok.bok_descityid=dc.dcty_id) join des_city_place dcp on (bok.bok_cityplaceid=dcp.dcplace_id) join sender s on (bok.bok_senderid=s.sendid) join recivers r on (bok.bok_reciverid=r.recvid) where bok.bok_descityid='$bok_descityid' AND bok_status='1'";	
+                                                        }
 							else
 							{
-								$sql="select * from booking bok join des_cities dc on (bok.bok_descityid=dc.dcty_id) join des_city_place dcp on (bok.bok_cityplaceid=dcp.dcplace_id) join sender s on (bok.bok_senderid=s.sendid) join recivers r on (bok.bok_reciverid=r.recvid) where bok.bok_descityid='$bok_descityid' OR bok_gst='$gst' OR bokdate='$date' OR boklrno='$lr' OR sendname='$s_name' OR recvname='$r_name' AND bok_status='1'";	
+								$sql="select * from booking bok join des_cities dc on (bok.bok_descityid=dc.dcty_id) join des_city_place dcp on (bok.bok_cityplaceid=dcp.dcplace_id) join sender s on (bok.bok_senderid=s.sendid) join recivers r on (bok.bok_reciverid=r.recvid) where bok.bok_descityid='$bok_descityid' AND bokdate BETWEEN  '$start_date' AND '$end_date' AND bok_status='1'";	
 							}
 							$result=mysql_query($sql) or die(mysql_error());
 							while($row=mysql_fetch_array($result))
@@ -208,6 +227,7 @@ function printDiv(divName) {
                                      <td class="a-center "> <?php echo $row["recvgstno"]; ?></td>  
                                       <td class="a-center "> <?php echo $row["dcplace_name"]; ?></td>   
                                      <td class="a-center "> <?php echo $row["bok_item"]; ?></td> 
+                                     <td class="a-center "> <?php echo $row["bok_paymode"]; ?></td>  
                                      <td class="a-center "> <?php echo $row["bok_total"]; ?></td>  
                                      <td class="a-center "> <?php echo $row["bok_gst"]; ?></td>
                                      <td class="a-center "> <?php echo $row["bok_memo"]; ?></td> 
